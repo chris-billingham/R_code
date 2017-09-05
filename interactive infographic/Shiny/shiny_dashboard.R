66fb9c368432824ebcbdb1f8dc2fd8603077a603
Tony Fly <jcflyingco@yahoo.com>
2017-09-06 01:48:23 +0800

    h

---
+++ interactive infographic/Shiny/shiny_dashboard.R
@@ -0,0 +1,13 @@
+#Shiny Dashboards 
+#https://www.youtube.com/watch?v=I5Z9WtTBZ_w
+#https://rstudio.github.io/shinydashboard/get_started.html
+#https://www.youtube.com/watch?v=sJl0EE_RE4o&list=PLH6mU1kedUy-aGYi-w1XqSiGtViFK9NpI
+library(shiny)
+library(plotly)
+library(dplyr)
+library(formattable)
+
+setwd('C:/Users/User/Desktop/Mission/R/R_code/interactive infographic/Shiny')
+getwd()
+
+
