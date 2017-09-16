bf42e0e051808f1737197f926f88e511e1033acc
Tony Fly <jcflyingco@yahoo.com>
2017-09-16 15:57:36 +0800

    w

---
+++ Get_Python/Book/get_python001.py
@@ -0,0 +1,13 @@
+a=1
+b=2
+
+import sys
+print (sys.version)
+
+
+#in command line:
+#conda version:conda --v
+#anaconda version:conda info
+
+#update conda :conda update conda
+#update anaconda :conda update anaconda
