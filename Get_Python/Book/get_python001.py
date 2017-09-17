c5119a5ebd8ab01c07a1617c45a11853eeed0c0a
Tony Fly <jcflyingco@yahoo.com>
2017-09-17 15:52:46 +0800

    w

--- Get_Python/Book/get_python001.py
+++ Get_Python/Book/get_python001.py
@@ -4,10 +4,15 @@ b=2
 import sys
 print (sys.version)
 
-
 #in command line:
 #conda version:conda --v
 #anaconda version:conda info
 
 #update conda :conda update conda
 #update anaconda :conda update anaconda
+
+import os
+os.chdir("C:\\Users\\tduan\\Desktop\\Mission\\R\\R_code\\Get_Python\\Book")
+
+import os 
+print(os.getcwd())
