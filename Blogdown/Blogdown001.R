46f5877bbf49b7eff4009441a900ddb602816bfa
Tony Fly <jcflyingco@yahoo.com>
2017-09-23 15:24:02 +0800

    w

---
+++ Blogdown/Blogdown001.R
@@ -0,0 +1,17 @@
+
+#https://gohugo.io/
+#https://www.youtube.com/watch?v=syWAKaj-4ck
+options(editor = "internal")
+#devtools::install_github('rstudio/blogdown')
+
+library(blogdown)
+
+setwd('C:/Users/tduan/Desktop/Mission/R/R_code/Blogdown/Bdown')
+getwd()
+
+#install_hugo()
+hugo_version()
+
+#install hugo
+new_site()
+
