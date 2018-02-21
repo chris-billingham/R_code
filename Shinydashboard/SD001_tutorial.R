621db59b058321a0eeef209e921335c0f2d631fd
Tony Fly <jcflyingco@yahoo.com>
2018-02-21 15:57:49 +0800

    h

---
+++ Shinydashboard/SD001_tutorial.R
@@ -0,0 +1,29 @@
+
+#https://rstudio.github.io/shinydashboard/get_started.html
+#https://www.youtube.com/watch?v=fUXBL5bk20M&list=PLH6mU1kedUy-aGYi-w1XqSiGtViFK9NpI&index=7
+
+
+#Video
+#Building Dashboards with Shiny – Tutorial
+#https://www.rstudio.com/resources/videos/building-dashboards-with-shiny-tutorial/
+
+
+#Dynamic Dashboards with Shiny
+#https://www.rstudio.com/resources/webinars/dynamic-dashboards-with-shiny/
+
+
+##########  package ################
+
+#install.packages("shinydashboard")
+#devtools::install_github("jcheng5/dashtutorial")
+
+
+library(shiny)
+library(shinydashboard)
+
+#########      ui      ##########################
+dashtutorial::summon()
+
+
+
+
