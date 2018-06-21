da520b81b05b29802a2e57a39f33d4c7d04114da
Tony Fly <jcflyingco@yahoo.com>
2018-06-21 10:10:54 +0800

    w

--- Datacamp/GCVM/GCVM in Python.py
+++ Datacamp/GCVM/GCVM in Python.py
@@ -256,7 +256,20 @@ library(tidyr)
 # 3 Visualization
 
 ## 3.1 matplotlib
-data;axis;plot
+
+
+#https://www.youtube.com/watch?v=q7Bo_J8x_dw&list=PLQVvvaa0QuDfefDfXb9Yf0la1fPDKluPF
+
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3],[5,7,4])
+
+plt.show()
+
+
+
+
+
 
 
 
